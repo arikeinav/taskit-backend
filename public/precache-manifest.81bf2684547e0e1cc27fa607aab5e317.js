da6a26540a76de4e174d366ba681b3dc7975d992
Shahar Sadof <shaharsadof22@gmail.com>
2020-09-29 11:27:47 +0300

    3

---
+++ public/precache-manifest.81bf2684547e0e1cc27fa607aab5e317.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4f739fa95a4b742cd18a4f060861f1e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "73b53a2b6d9861807daa",
+    "url": "/static/css/2.8d4cdce2.chunk.css"
+  },
+  {
+    "revision": "bb90c54d62b36a223c46",
+    "url": "/static/css/main.b7e93101.chunk.css"
+  },
+  {
+    "revision": "73b53a2b6d9861807daa",
+    "url": "/static/js/2.0bc95f1d.chunk.js"
+  },
+  {
+    "revision": "a2b4e87b4503807a540d337bb5989c7e",
+    "url": "/static/js/2.0bc95f1d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb90c54d62b36a223c46",
+    "url": "/static/js/main.5147d633.chunk.js"
+  },
+  {
+    "revision": "779a449811dc4857cd1b",
+    "url": "/static/js/runtime-main.16448157.js"
+  },
+  {
+    "revision": "2b9b7690ea41eca720fedddff070e153",
+    "url": "/static/media/DancingScript-Medium.2b9b7690.ttf"
+  },
+  {
+    "revision": "c8b6e083af3f94009801989c3739425e",
+    "url": "/static/media/Montserrat-Medium.c8b6e083.ttf"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "b1ce4cd13e96c14fa23147273b48d523",
+    "url": "/static/media/NanumBrushScript-Regular.b1ce4cd1.ttf"
+  },
+  {
+    "revision": "58aef543c97bbaf6a9896e8484456d98",
+    "url": "/static/media/Roboto-Medium.58aef543.ttf"
+  },
+  {
+    "revision": "56a9676b142056f818bf7fcb8c246350",
+    "url": "/static/media/Vidaloka-Regular.56a9676b.ttf"
+  },
+  {
+    "revision": "28b9e4d60aa5f4bf2b45c343baa1d077",
+    "url": "/static/media/seaa.28b9e4d6.jpg"
+  }
+]);
